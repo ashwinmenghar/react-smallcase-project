4db3d1ad2e7a09dcae9b88d514865b48d471bb74
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-29 14:38:16 +0530

    Rename investmentAmountOptions to INVESTMENTAMOUNTOPTIONS and add SUBSCRIPTIONOPTIONS constant

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,13 @@
-export const investmentAmountOptions = [
+export const INVESTMENTAMOUNTOPTIONS = [
   { id: "any", label: "Any", value: "any" },
   { id: "under-5000", label: "Under ₹5,000", value: "5000" },
   { id: "under-25000", label: "Under ₹25,000", value: "25000" },
   { id: "under-50000", label: "Under ₹50,000", value: "50000" },
+];
+
+
+export const SUBSCRIPTIONOPTIONS = [
+  { label: "Show all", value: null },
+  { label: "Free access", value: "public" },
+  { label: "Fee based", value: "private" },
 ];
