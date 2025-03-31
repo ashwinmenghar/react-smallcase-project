5cd143e1924ed46c655c4678fce123598248174b
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-31 10:33:05 +0530

    Add time period options and sorting functionality to investment data

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -5,9 +5,27 @@ export const INVESTMENTAMOUNTOPTIONS = [
   { id: "under-50000", label: "Under ₹50,000", value: "50000" },
 ];
 
-
 export const SUBSCRIPTIONOPTIONS = [
   { label: "Show all", value: null },
   { label: "Free access", value: "public" },
   { label: "Fee based", value: "private" },
 ];
+
+export const TIME_PERIOD_BTN = {
+  0: { key: "monthly", value: "1M", label: "1M Returns" },
+  1: { key: "halfyearly", value: "6M", label: "6M Returns" },
+  2: { key: "yearly", value: "1Y", label: "1Y CAGR" },
+  3: { key: "threeYear", value: "3Y", label: "3Y CAGR" },
+  4: { key: "fiveYear", value: "5Y", label: "5Y CAGR" },
+};
+
+export const SORT = {
+  popularity: "Popularity",
+  minInvestAmount: "Minimum Amount",
+  rebalanceDate: "Recently Rebalanced",
+};
+
+export const ORDERBY = {
+  0: { key: "high-low", value: "High - Low", label: "H -> L" },
+  1: { key: "low-high", value: "Low - High", label: "L -> H" },
+};
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,12 +1,66 @@
-export const sortSmallcases = (data) => {
-  return data.sort((a, b) => {
-    const rankA = a?.brokerMeta?.flags?.popular?.rank ?? Infinity;
-    const rankB = b?.brokerMeta?.flags?.popular?.rank ?? Infinity;
+export const sortSmallcases = (data, sort) => {
+  console.log(sort);
 
-    return rankA - rankB || a.info.name.localeCompare(b.info.name);
+  Object.entries(sort).every(([, value]) => {
+    return data.sort((a, b) => {
+      switch (value) {
+        case "popularity": {
+          const rankA = a?.brokerMeta?.flags?.popular?.rank ?? Infinity;
+          const rankB = b?.brokerMeta?.flags?.popular?.rank ?? Infinity;
+
+          return rankA - rankB || a.info.name.localeCompare(b.info.name);
+        }
+
+        case "minInvestAmount": {
+          const minInvestAmountOfA = a?.stats?.minInvestAmount ?? Infinity;
+          const minInvestAmountOfB = b?.stats?.minInvestAmount ?? Infinity;
+
+          return minInvestAmountOfA - minInvestAmountOfB;
+        }
+
+        case "rebalanceDate": {
+          const dateA = new Date(a?.info?.lastRebalanced ?? 0);
+          const dateB = new Date(b?.info?.lastRebalanced ?? 0);
+          return dateB - dateA;
+        }
+        case "monthly":
+        case "halfyearly":
+        case "yearly":
+        case "threeYear":
+        case "fiveYear": {
+          const periods = {
+            monthly: 1,
+            halfyearly: 1,
+            yearly: 1,
+            threeYear: 3,
+            fiveYear: 5,
+          };
+
+          const period = periods[value];
+          const calculateCAGROfA = calculateCAGR(
+            a?.stats?.indexValue ?? 0,
+            a?.stats?.returns?.[value] ?? 0,
+            period
+          ).toFixed(2);
+          const calculateCAGROfB = calculateCAGR(
+            b?.stats?.indexValue ?? 0,
+            b?.stats?.returns?.[value] ?? 0,
+            period
+          ).toFixed(2);
+
+          a.stats["calculatedCAGR"] = parseFloat(calculateCAGROfA);
+          b.stats["calculatedCAGR"] = parseFloat(calculateCAGROfB);
+
+          return sort.orderby === "low-high"
+            ? parseFloat(calculateCAGROfA) - parseFloat(calculateCAGROfB)
+            : parseFloat(calculateCAGROfB) - parseFloat(calculateCAGROfA);
+        }
+        default:
+          break;
+      }
+    });
   });
 };
-
 export const truncateText = (text, maxLength = 100) => {
   return text.length > maxLength
     ? text.slice(0, maxLength).trimEnd() + "..."
@@ -19,4 +73,10 @@ export const isLessThanOneYearOld = (dateString) => {
   oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
 
   return givenDate > oneYearAgo;
+};
+
+export const calculateCAGR = (finalValue, returnRate, years) => {
+  if (years <= 0) return 0;
+  const initialValue = finalValue / (1 + returnRate); // Reverse calculate initial value
+  return ((finalValue / initialValue) ** (1 / years) - 1) * 100;
 };
